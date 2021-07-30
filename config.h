ac66c5d0fb35230c94991a8b1b4d8ef7af0d18c5
Kyle Se <kyle@skynet.localdomain>
2021-07-30 14:27:56 +1000

    commit

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *symbol_1 = "<";
 static const char *symbol_2 = ">";
 
-static const unsigned int baralpha = 0xff; 
+static const unsigned int baralpha = 0xef; 
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -46,7 +46,7 @@ static unsigned int columns    = 0;
 static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 static unsigned int min_lineheight = 35;
 static unsigned int maxhist    = 15;
-static int histnodup           = 0;	/* if 0, record repeated histories */
+static int histnodup           = 1;	/* if 0, record repeated histories */
 
 /*
  * Characters not considered part of a word while deleting words
