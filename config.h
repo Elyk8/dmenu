9d0507c45649f8bfe95ed99a98a785d0f811edaa
Kyle Se <kyle@skynet.localdomain>
2021-08-02 20:02:36 +1000

    commit

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *symbol_1 = "<";
 static const char *symbol_2 = ">";
 
-static const unsigned int baralpha = 0xef; 
+static const unsigned int baralpha = 0xd0; 
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -45,8 +45,8 @@ char *colors[][2] = {
 static unsigned int lines      = 0;
 /* -g option; if nonzero, dmenu uses a grid comprised of columns and lines */
 static unsigned int columns    = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
-static unsigned int min_lineheight = 30;
+static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
+static unsigned int min_lineheight = 0;
 static unsigned int maxhist    = 15;
 static int histnodup           = 1;	/* if 0, record repeated histories */
 
@@ -58,7 +58,7 @@ static const char startpipe[] = "#";
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 3;
+static unsigned int border_width = 0;
 
 /*
  * Use prefix matching by default; can be inverted with the -x flag.
