f455efa92cc871fda914d8f15e17ecc44b320e52
elyk <kylese58@gmail.com>
2022-03-08 09:26:10 +1100

    commit

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom */
 static int opacity = 0;                     /* -o  option; if 0, then alpha is disabled */
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int center = 0;                      /* -c  option; if 0, dmenu won't be centered on the screen */
@@ -45,7 +45,7 @@ char *colors[][2] = {
 static unsigned int lines      = 0;
 /* -g option; if nonzero, dmenu uses a grid comprised of columns and lines */
 static unsigned int columns    = 0;
-static unsigned int lineheight = 40;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 static unsigned int min_lineheight = 0;
 static unsigned int maxhist    = 15;
 static int histnodup           = 1;	/* if 0, record repeated histories */
